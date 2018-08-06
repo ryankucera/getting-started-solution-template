eaafd6efa764753e09defcb281404b7c0cf8de1c
Ryan Kucera <ryankucera@exosite.com>
2018-08-06 15:03:26 -0500

    Update example.lua

--- endpoints/example.lua
+++ endpoints/example.lua
@@ -4,5 +4,5 @@ print("Creating a new user")
 --#ENDPOINT GET /api/user/{userId}
 print("Fetch a given user" .. request.parameters.userId)
 
---#ENDPOINT GET /test/{service}
-return Config.getParameters({service=request.parameters.service})
+--#ENDPOINT GET /test/{serviceName}
+-- return Config.getParameters({service=request.parameters.serviceName})
